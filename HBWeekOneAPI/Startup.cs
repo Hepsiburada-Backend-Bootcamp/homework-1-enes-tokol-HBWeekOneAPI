85db56ba923a573af032d126119561c767ef1f48
Enes <enestokol@gmail.com>
2021-09-22 11:57:55 +0300

    Add routing

--- HBWeekOneAPI/Startup.cs
+++ HBWeekOneAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace HBWeekOneAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
