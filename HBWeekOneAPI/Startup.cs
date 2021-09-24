b117e22ba1f8212ecbcf0a32d230864bcff5124a
Enes <enestokol@gmail.com>
2021-09-24 11:50:12 +0300

    Add Entity Framework and Mysql connector library

--- HBWeekOneAPI/Startup.cs
+++ HBWeekOneAPI/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HBWeekOneAPI.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,10 @@ namespace HBWeekOneAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "HBWeekOneAPI", Version = "v1"});
             });
+            
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(Configuration["ConnectionStrings:Default"], serverVersion));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
