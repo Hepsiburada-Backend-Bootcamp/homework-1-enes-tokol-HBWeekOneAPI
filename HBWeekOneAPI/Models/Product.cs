723901c1c55e95b2c423ddec61de5b67340fab1a
Enes <enestokol@gmail.com>
2021-09-24 11:50:53 +0300

    Add product model

---
+++ HBWeekOneAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HBWeekOneAPI.Models
+{
+    public class Product:IBaseModel
+    {
+        #region Scalar Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #endregion
+    }
+}
