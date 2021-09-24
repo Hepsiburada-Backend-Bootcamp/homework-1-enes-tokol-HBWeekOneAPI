64fa2be2c393ee21f73fd97b824fe0a920dfc863
Enes <enestokol@gmail.com>
2021-09-24 11:51:22 +0300

    Add Seed data

---
+++ HBWeekOneAPI/EntityFramework/Seeds/ProductSeed.cs
@@ -0,0 +1,22 @@
+﻿using HBWeekOneAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HBWeekOneAPI.EntityFramework.Seeds
+{
+    public class ProductSeed:IEntityTypeConfiguration<Product>
+    {
+        public ProductSeed()
+        {
+            
+        }
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1,Name = "Sony Xperia L1"},
+                new Product { Id = 2,Name = "Huawei Mate 9 "},
+                new Product { Id = 3,Name = "Iphone 13"},
+                new Product { Id = 4,Name = "Redmi Note 10"});
+        }
+    }
+}
