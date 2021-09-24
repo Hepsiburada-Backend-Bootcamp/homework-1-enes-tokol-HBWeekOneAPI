b8a8b73d56c143d9afa9467fdd80029306cf61b8
Enes <enestokol@gmail.com>
2021-09-24 11:52:31 +0300

    Add constant messages

---
+++ HBWeekOneAPI/Constants/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace HBWeekOneAPI.Constants
+{
+    public static class Message
+    {
+        public const string ProductNotFound = "Product Not Found";
+        public const string ProductAlreadyAdded = "Product Is Exist";
+        public const string ProductNameNotUnique = "Product Name Is Exist";
+    }
+}
