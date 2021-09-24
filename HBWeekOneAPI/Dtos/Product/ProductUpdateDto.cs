5dd412321e6dce1b905e9080e735458dcef670ae
Enes <enestokol@gmail.com>
2021-09-24 11:52:23 +0300

    Add Product DTOs for Create and Update operations

---
+++ HBWeekOneAPI/Dtos/Product/ProductUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HBWeekOneAPI.Dtos.Product
+{
+    public class ProductUpdateDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(2, ErrorMessage = "Name cannot be less than 2"),MaxLength(255,ErrorMessage = "Name cannot be greater than 255")]
+        public string Name { get; set; }
+    }
+}
