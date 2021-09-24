c2a366b678020eae88baf52a2bc70d4644f33775
Enes <enestokol@gmail.com>
2021-09-24 11:51:12 +0300

    Add Product table configuration

---
+++ HBWeekOneAPI/EntityFramework/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using HBWeekOneAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HBWeekOneAPI.EntityFramework.Configurations
+{
+    public class ProductConfiguration:IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("products");
+            
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id).UseMySqlIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(255)
+                .IsRequired();
+        }
+    }
+}
