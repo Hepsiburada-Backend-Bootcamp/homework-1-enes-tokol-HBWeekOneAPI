a4a5b23354e11c989a9a120ef8f28fa045608d68
Enes <enestokol@gmail.com>
2021-09-24 11:51:36 +0300

    Add Migrations

---
+++ HBWeekOneAPI/EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HBWeekOneAPI.EntityFramework.Configurations;
+using HBWeekOneAPI.EntityFramework.Seeds;
+using HBWeekOneAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HBWeekOneAPI.EntityFramework
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductSeed());
+        }
+    }
+}
---
+++ HBWeekOneAPI/Migrations/20210922134237_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HBWeekOneAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Sony Xperia L1" },
+                    { 2, "Huawei Mate 9 " },
+                    { 3, "Iphone 13" },
+                    { 4, "Redmi Note 10" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
